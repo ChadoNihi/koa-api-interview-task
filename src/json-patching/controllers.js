c2e690f503255ee0bccbf63df62d456bd137ec84
ChadoNihi <chadoishere@gmail.com>
2018-02-27 16:37:27 +0200

    omit stack trace in logs

--- src/json-patching/controllers.js
+++ src/json-patching/controllers.js
@@ -1,31 +1,31 @@
 import {
-	applyOperation,
-	applyReducer
+  applyOperation,
+  applyReducer
 } from "fast-json-patch";
 
 export const postPatch = async (ctx) => {
-	const {
-		patch,
-		target
-	} = ctx.request.body;
+  const {
+    patch,
+    target
+  } = ctx.request.body;
 
-	// if falsy or {}
-	if (!patch || (Object.keys(patch).length === 0 && patch.constructor === Object)) {
-		ctx.body = {
-			result: target
-		};
-	} else {
-		try {
-			ctx.body = {
-				result: (
-					Array.isArray(patch) ?
-						patch.reduce(applyReducer, target) :
-						applyOperation(target, patch).newDocument
-				)
-			};
-		} catch (e) {
-			ctx.log.info(e);
-			ctx.throw(400);
-		}
-	}
+  // if falsy or {}
+  if (!patch || (Object.keys(patch).length === 0 && patch.constructor === Object)) {
+    ctx.body = {
+      result: target
+    };
+  } else {
+    try {
+      ctx.body = {
+        result: (
+          Array.isArray(patch) ?
+          patch.reduce(applyReducer, target) :
+          applyOperation(target, patch).newDocument
+        )
+      };
+    } catch (e) {
+      ctx.log.info(e.message);
+      ctx.throw(400);
+    }
+  }
 };
